fedc5e5dd4c1580732970297f7215850070a83bc
Andrew <expfront@gmail.com>
2015-10-08 15:04:47 +0300

    try

--- scripts/public.js
+++ scripts/public.js
@@ -54,17 +54,18 @@ var Comment = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
     return (
-      <div>
-        {
-          this.props.data.map(function (name) {
-           return (
-             <h3>{name}</h3>
-           );
-          })
-        }
+      <div className="commentList">
+        {commentNodes}
       </div>
-    )
+    );
   }
 });
 

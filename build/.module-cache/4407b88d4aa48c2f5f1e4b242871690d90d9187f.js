4fb76ace0f4eddaeefe3af3da8494769dcc00432
Andrew <expfront@gmail.com>
2015-10-08 12:49:50 +0300

    try

--- build/.module-cache/4407b88d4aa48c2f5f1e4b242871690d90d9187f.js
+++ build/.module-cache/4407b88d4aa48c2f5f1e4b242871690d90d9187f.js
@@ -1,5 +1,5 @@
 
-var Comment = React.createClass({
+var Comment = React.createClass({displayName: "Comment",
 
   getInitialState: function() {
     return {data: []}
@@ -39,34 +39,34 @@ var Comment = React.createClass({
 
   render: function() {
     return (
-      <div>
-        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+      React.createElement("div", null, 
+        React.createElement(CommentForm, {onCommentSubmit: this.handleCommentSubmit}), 
 
-        <CommentList loadCommentFromServer={this.loadCommentFromServer} data={this.state.data} />
-      </div>  
+        React.createElement(CommentList, {loadCommentFromServer: this.loadCommentFromServer, data: this.state.data})
+      )  
     );
   }
 });
 
-var CommentList = React.createClass({
+var CommentList = React.createClass({displayName: "CommentList",
   render: function() {
     return (
-      <div>
-      {this.props.loadCommentsFromServer}
-        {
+      React.createElement("div", null, 
+
+        
           this.props.data.map(function (name) {
            return (
-             <h3>{name} </h3>
+             React.createElement("h3", null, name, " ")
            );
           })
-        }
-      </div>
+        
+      )
     )
   }
 });
 
 
-var CommentForm = React.createClass({
+var CommentForm = React.createClass({displayName: "CommentForm",
 
   handleSubmit: function(e) {
     e.preventDefault();
@@ -85,11 +85,11 @@ var CommentForm = React.createClass({
 
   render: function() {
     return (
-      <form onSubmit={this.handleSubmit} onCommentSubmit={this.props.handleCommentSubmit} >
-        <input type="text" ref="author" placeholder="Your name: "/>
-        <input type="text" ref="text" placeholder="Your text: "/>
-        <input type="submit" value="Post" />
-      </form>
+      React.createElement("form", {onSubmit: this.handleSubmit, onCommentSubmit: this.props.handleCommentSubmit}, 
+        React.createElement("input", {type: "text", ref: "author", placeholder: "Your name: "}), 
+        React.createElement("input", {type: "text", ref: "text", placeholder: "Your text: "}), 
+        React.createElement("input", {type: "submit", value: "Post"})
+      )
     );
 
   }
@@ -97,7 +97,7 @@ var CommentForm = React.createClass({
 
 var onLoad= function() {
   React.render(
-    <Comment url="datas.json" />,
+    React.createElement(Comment, {url: "datas.json"}),
     document.getElementById('content')
   );
 }

5b58f22f2f7178fe02f33687be1ad7a750dedec3
Andrew <expfront@gmail.com>
2015-10-15 00:09:48 +0300

    Edit paths.

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,9 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
       }
     ]
   }
